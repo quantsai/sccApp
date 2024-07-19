1d0e718f7bbf594bc805bac1919c10620a299f2e
caiquan <396022671@qq.com>
2024-07-19 09:53:23 +0800

    Initial Commit

---
+++ sccApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sccApp
+//
+//  Created by 蔡铨 on 2024/7/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sccApp/sccAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sccAppApp.swift
+//  sccApp
+//
+//  Created by 蔡铨 on 2024/7/19.
+//
+
+import SwiftUI
+
+@main
+struct sccAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
