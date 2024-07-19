d1b7f9dfe2c5034e1ab2e7e28a7de36ae7b6a89b
caiquan <396022671@qq.com>
2024-07-19 16:25:20 +0800

    test5

--- sccApp/ContentView.swift
+++ sccApp/ContentView.swift
@@ -10,26 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-        
-            // FIXME: sss
-            // icon
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.blue)
-                Text("Hello, world!!")
-                    .font(.largeTitle)
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.primarySecond)
-                
-            Image("2")
-                .renderingMode(.original)
-                .aspectRatio(contentMode: .fit)
-                
-            Label(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, systemImage: /*@START_MENU_TOKEN@*/"42.circle"/*@END_MENU_TOKEN@*/)
-
-
-            
-            
+                Text("Hello, world Cai!")
         }
         .padding()
     }
